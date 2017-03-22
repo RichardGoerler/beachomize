Added selection menu for mmr calculation method. Tested mmr methods a bit and fixed bugs in MMR methods. Fixed some other bugs. Added command line option to settings menu.

gui.py
@@ -199,23 +199,55 @@ class WelcomeWindow(dialog.Dialog):
         w.grid(row=0, column=0, padx=int(self.gui.default_size/2), pady=int(self.gui.default_size/2))
         w = tk.Button(box, text="Laden", width=10, command=self.ok, bg="#EDEEF3")
         w.grid(row=0, column=1, padx=int(self.gui.default_size/2), pady=int(self.gui.default_size/2))
+
         self.cvar = tk.IntVar()
         self.cvar.set(3)
-        tk.Label(box, text="Anzahl Felder:", bg="#EDEEF3").grid(row=1, column=0)
-        tk.OptionMenu(box, self.cvar, 1, 2, 3, 4, 5).grid(row=2, column=0)
-        tk.Label(box, text="Starzeit und Dauer:", bg="#EDEEF3").grid(row=3, column=0, pady=int(self.gui.default_size/2))
+        tk.Label(box, text="Anzahl Felder:", bg="#EDEEF3").grid(row=1, column=0, pady=int(self.gui.default_size/2))
+        self.cmenu = tk.OptionMenu(box, self.cvar, 1, 2, 3, 4, 5)
+        self.cmenu.config(bg="#EDEEF3")
+        self.cmenu["menu"].config(bg="#EDEEF3")
+        self.cmenu.grid(row=2, column=0)
+        tk.Label(box, text="Starzeit und Dauer:", bg="#EDEEF3").grid(row=3, column=0, pady=int(self.gui.default_size / 2))
         self.time_var = tk.StringVar()
         self.time_var.set("2100")
-        self.time_entry = tk.Entry(box, textvariable=self.time_var, width=5)
+        self.time_entry = tk.Entry(box, textvariable=self.time_var, width=5, bg="#EDEEF3")
         self.time_entry.grid(row=4, column=0)
         self.duration_var = tk.StringVar()
         self.duration_var.set("300")
-        self.duration_entry = tk.Entry(box, textvariable=self.duration_var, width=5)
-        self.duration_entry.grid(row=5, column=0, pady=int(self.gui.default_size/2))
+        self.duration_entry = tk.Entry(box, textvariable=self.duration_var, width=5, bg="#EDEEF3")
+        self.duration_entry.grid(row=5, column=0, pady=int(self.gui.default_size / 2))
+        self.mmrvar = tk.StringVar()
+        self.mmrvar.set(turnier.MMR_METHODS[-1])
+        tk.Label(box, text="MMR-Methode:", bg="#EDEEF3").grid(row=6, column=0, pady=int(self.gui.default_size / 2))
+        self.mmrmenu = tk.OptionMenu(box, self.mmrvar, command=self.update_mmr, *turnier.MMR_METHODS)
+        self.mmrmenu["menu"].config(bg="#EDEEF3")
+        self.mmrmenu.config(bg="#EDEEF3")
+        self.mmrmenu.grid(row=7, column=0)
+        self.mmrtagvar = tk.StringVar()
+        self.mmrtagvar.set(turnier.MMR_TAGS[0])
+        self.taglabel = tk.Label(box, text="MMR-Tags:", bg="#EDEEF3")
+        self.taglabel.grid(row=8, column=0, pady=int(self.gui.default_size / 2))
+        self.tagmenu = tk.OptionMenu(box, self.mmrtagvar, *turnier.MMR_METHODS)
+        self.tagmenu.config(bg="#EDEEF3")
+        self.tagmenu["menu"].config(bg="#EDEEF3")
+        self.tagmenu.grid(row=9, column=0)
+        self.tag_rendered = True
 
         self.bind('<Return>', self.new)
 
         box.pack()
+
+    def update_mmr(self, event=None):
+        if "streak" in self.mmrvar.get():
+            if not self.tag_rendered:
+                self.taglabel.grid(row=8, column=0, pady=int(self.gui.default_size / 2))
+                self.tagmenu.grid(row=9, column=0)
+                self.tag_rendered = True
+        else:
+            if self.tag_rendered:
+                self.taglabel.grid_forget()
+                self.tagmenu.grid_forget()
+                self.tag_rendered = False
 
     def validate(self):
         try:
@@ -264,7 +296,8 @@ class WelcomeWindow(dialog.Dialog):
         self.update_idletasks()
 
         names, mmr = self.gui.in_players()
-        self.gui.tur = turnier.Turnier(names, mmr, courts=self.cvar.get(), start_time=starttime, duration=duration)
+        self.gui.tur = turnier.Turnier(names, mmr, courts=self.cvar.get(), start_time=starttime, duration=duration,
+                                       matchmaking=turnier.MMR_METHODS.index(self.mmrvar.get()), matchmaking_tag=turnier.MMR_TAGS.index(self.mmrtagvar.get()))
 
         self.cancel()
 
@@ -306,11 +339,16 @@ class GameNumberWindow(dialog.Dialog):
 
 class SettingsWindow(dialog.Dialog):
     def body(self, master):
-        tk.Label(master, text="Schriftgröße:", bg="#EDEEF3").pack(side=tk.LEFT, padx=int(self.gui.default_size/2), pady=int(self.gui.default_size/2))
+        tk.Button(master, text="Kommandozeile", command=self.cmd, bg="#EDEEF3").grid(row=0, columnspan=2, pady = self.gui.default_size)
+        tk.Label(master, text="Schriftgröße:", bg="#EDEEF3").grid(row=1, column=0, padx=int(self.gui.default_size/2), pady=int(self.gui.default_size/2))
         self.default_size_spin = tk.Spinbox(master, width=5, from_=5, to=30, bg="#EDEEF3")
         self.default_size_spin.delete(0, "end")
         self.default_size_spin.insert(0, int(np.round(self.gui.default_size/self.gui.hdfactor)))
-        self.default_size_spin.pack(side=tk.LEFT)
+        self.default_size_spin.grid(row=1, column=1)
+        return self.default_size_spin
+
+    def cmd(self, event=None):
+        _ = CommandWindow(self.master, self.gui)
 
     def check_value(self):
         try:
@@ -380,6 +418,27 @@ class SettingsWindow(dialog.Dialog):
 
         self.parent.focus_set()
         self.destroy()
+
+class CommandWindow(dialog.Dialog):
+    def header(self, master):
+        pass
+
+    def body(self, master):
+        self.cm = tk.StringVar()
+        tk.Entry(master, textvariable=self.cm, bg="#EDEEF3").pack()
+
+    def validate(self):
+        try:
+            print(eval(self.cm.get()))
+        except Exception as e:
+            try:
+                exec(self.cm.get())
+            except:
+                print(e)
+                tkMessageBox.showwarning("Fehler", "eval und exec fehlgeschlagen. Bei eval aufgetretene Exception ausgegeben.")
+                return 0
+        return 1
+
 
 class GUI:
     def __init__(self):
@@ -660,7 +719,7 @@ class GUI:
                 mmr_sorted = self.tur.players.mmr[team_indices]
                 mmr_mean = np.mean(mmr_sorted.astype(float), axis=1)
                 for i in range(len(team_indices) / 2):
-                    self.mmr_labels[i]["text"] = "{:.2f}/{:.2f} /ø{:.2f}) - {:.2f}/{:.2f} /ø{:.2f})".format(mmr_sorted[2 * i][0], mmr_sorted[2 * i][1], mmr_mean[2 * i], mmr_sorted[2 * i + 1][0], mmr_sorted[2 * i + 1][1], mmr_mean[2 * i + 1])
+                    self.mmr_labels[i]["text"] = "{:.1f}/{:.1f} (ø{:.2f}) - {:.1f}/{:.1f} (ø{:.2f})".format(mmr_sorted[2 * i][0], mmr_sorted[2 * i][1], mmr_mean[2 * i], mmr_sorted[2 * i + 1][0], mmr_sorted[2 * i + 1][1], mmr_mean[2 * i + 1])
             else:
                 for i in range(len(team_indices) / 2):
                     self.mmr_labels[i]["text"] = ""

turnier2.py
@@ -9,6 +9,10 @@ MMR_FLAT = 1
 MMR_FLAT_STREAK = 2
 MMR_DIFF = 3
 MMR_DIFF_STREAK = 4
+MMR_METHODS = ["none", "flat", "flat streak", "diff", "diff streak"]
+MMR_TAG_CUM = 0
+MMR_TAG_IND = 1
+MMR_TAGS = ["cumulative", "individual"]
 
 def load(gui, filename="saved.p"):
     with open(filename, 'rb') as f:
@@ -59,7 +63,7 @@ class Turnier:
             f %= self.p
             i += 1
 
-    def __init__(self, names, mmr, courts=3, start_time=2100, duration=300, matchmaking=MMR_DIFF_STREAK, display_mmr=False, orgatime=3):
+    def __init__(self, names, mmr, courts=3, start_time=2100, duration=300, matchmaking=MMR_DIFF_STREAK, matchmaking_tag=MMR_TAG_CUM, display_mmr=False, orgatime=3):
         self.init_mmr = mmr
         self.start_time = start_time
         self.duration = duration
@@ -72,7 +76,8 @@ class Turnier:
         self.players["score"] = self.players["diff"] = self.players["points"] = self.players["wait"] = self.players["mmr_tag_w"] = self.players["mmr_tag_l"] = [0]*self.p
         self.players_copy = None
         self.partner_matrix = np.zeros((self.p, self.p))
-        self.matchmaking = matchmaking   # whether to sort teams before making matches (True) or to randomize (False)
+        self.matchmaking = matchmaking   # whether to sort teams before making matches (True) or to randomize (False). If True, different methods apply (see constants)
+        self.matchmaking_tag = matchmaking_tag   #whether to update mmr tag (streak information) after each set or after each game with cumulative score-difference
         self.display_mmr = display_mmr   # whether to show mmr when announcing games
         self.orgatime = orgatime
         self.c = courts
@@ -237,11 +242,13 @@ class Turnier:
         team_indices_sorted = self.games[game_number]
         self.results[game_number] = res
         for ci, court_results in enumerate(self.results[game_number]):  # ci = court index
-            pl = self.players[team_indices_sorted[2 * ci:2 * ci + 2]] #I know the values are copied here, but I buy myself readibility in mmr lines by keeping it short and pay with efficiency
+            cumdiff = 0
             for set_result in court_results:   # si = set index
+                pl = self.players[team_indices_sorted[2 * ci:2 * ci + 2]]  # I know the values are copied here, but I buy myself readability in mmr lines by keeping them short and pay with efficiency
                 score1 = set_result[0]
                 score2 = set_result[1]
                 scorediff = score1-score2
+                cumdiff += scorediff
 
                 # generate index 0: lost, 1: draw, 2: won. index default score array with that index
                 self.players[team_indices_sorted[2 * ci, 0]].score += [0, 0, 1][np.sign(scorediff) + 1]
@@ -260,18 +267,6 @@ class Turnier:
                 self.players[team_indices_sorted[2 * ci + 1, 0]].points += score2
                 self.players[team_indices_sorted[2 * ci + 1, 1]].points += score2
 
-                # mmr_tag_w keeps track of number of wins {0,1,2} and mmr_tag_l accordingly for losses
-                # mmr_tag_w is increased in case of win, mmr_tag_l is decreased and vice versa. In case of tie both are decreased
-                self.players[team_indices_sorted[2 * ci, 0]].mmr_tag_w += 1 if scorediff > 0 else -1
-                self.players[team_indices_sorted[2 * ci, 1]].mmr_tag_w += 1 if scorediff > 0 else -1
-                self.players[team_indices_sorted[2 * ci + 1, 0]].mmr_tag_w += 1 if scorediff < 0 else -1
-                self.players[team_indices_sorted[2 * ci + 1, 1]].mmr_tag_w += 1 if scorediff < 0 else -1
-
-                self.players[team_indices_sorted[2 * ci, 0]].mmr_tag_l += 1 if scorediff < 0 else -1
-                self.players[team_indices_sorted[2 * ci, 1]].mmr_tag_l += 1 if scorediff < 0 else -1
-                self.players[team_indices_sorted[2 * ci + 1, 0]].mmr_tag_l += 1 if scorediff > 0 else -1
-                self.players[team_indices_sorted[2 * ci + 1, 1]].mmr_tag_l += 1 if scorediff > 0 else -1
-
                 if self.matchmaking == MMR_DIFF_STREAK:
                     # according to mmr_tags scorediff is multiplied with 0.3, 0.6 or 1. When on winning/losing streak, game outcomes are more strongly weighted
                     # this is intended to decrease the influence of random fluctuations in performance
@@ -281,7 +276,7 @@ class Turnier:
                     self.players[team_indices_sorted[2 * ci + 1, 1]].mmr -= scorediff*[0.3, 0.6, 1][pl[1,1].mmr_tag_w] if scorediff < 0 else scorediff*[0.3, 0.6, 1][pl[1,1].mmr_tag_l]
 
                 if self.matchmaking == MMR_FLAT_STREAK:
-                    #same as diff streak, but is multiplied with one, irresepctive of points dfference
+                    #same as diff streak, but is multiplied with one, irrespective of points dfference
                     self.players[team_indices_sorted[2 * ci, 0]].mmr += [0.3, 0.6, 1][pl[0, 0].mmr_tag_w] if scorediff > 0 else [-0.3, -0.6, -1][pl[0, 0].mmr_tag_l]
                     self.players[team_indices_sorted[2 * ci, 1]].mmr += [0.3, 0.6, 1][pl[0, 1].mmr_tag_w] if scorediff > 0 else [-0.3, -0.6, -1][pl[0, 1].mmr_tag_l]
                     self.players[team_indices_sorted[2 * ci + 1, 0]].mmr -= [0.3, 0.6, 1][pl[1, 0].mmr_tag_w] if scorediff < 0 else [-0.3, -0.6, -1][pl[1, 0].mmr_tag_l]
@@ -301,15 +296,34 @@ class Turnier:
                     self.players[team_indices_sorted[2 * ci + 1, 0]].mmr += [-1, 0, 1][np.sign(-scorediff) + 1]
                     self.players[team_indices_sorted[2 * ci + 1, 1]].mmr += [-1, 0, 1][np.sign(-scorediff) + 1]
 
-                for ti in range(0,2):
-                    for pi in range(0,2):
-                        self.players[team_indices_sorted[2 * ci+ti, pi]].mmr_tag_w = min(max(self.players[team_indices_sorted[2 * ci+ti, pi]].mmr_tag_w,0), 2)
-                        self.players[team_indices_sorted[2 * ci + ti, pi]].mmr_tag_l = min(max(self.players[team_indices_sorted[2 * ci+ti, pi]].mmr_tag_l,0), 2)
+                if self.matchmaking_tag == MMR_TAG_IND:
+                    self._update_mmr_tag(ci, team_indices_sorted, scorediff)
+
+            if self.matchmaking_tag == MMR_TAG_CUM:
+                self._update_mmr_tag(ci, team_indices_sorted, cumdiff)
 
         if self.state == 1:
             self.state = 2
         with open("saved.p", 'wb') as f:
             pickle.dump(self, f)
+
+    def _update_mmr_tag(self, ci, teams, diff):
+        # mmr_tag_w keeps track of number of wins {0,1,2} and mmr_tag_l accordingly for losses
+        # mmr_tag_w is increased in case of win, mmr_tag_l is decreased and vice versa. In case of tie both are decreased
+        self.players[teams[2 * ci, 0]].mmr_tag_w += 1 if diff > 0 else -1
+        self.players[teams[2 * ci, 1]].mmr_tag_w += 1 if diff > 0 else -1
+        self.players[teams[2 * ci + 1, 0]].mmr_tag_w += 1 if diff < 0 else -1
+        self.players[teams[2 * ci + 1, 1]].mmr_tag_w += 1 if diff < 0 else -1
+
+        self.players[teams[2 * ci, 0]].mmr_tag_l += 1 if diff < 0 else -1
+        self.players[teams[2 * ci, 1]].mmr_tag_l += 1 if diff < 0 else -1
+        self.players[teams[2 * ci + 1, 0]].mmr_tag_l += 1 if diff > 0 else -1
+        self.players[teams[2 * ci + 1, 1]].mmr_tag_l += 1 if diff > 0 else -1
+
+        for ti in range(0, 2):
+            for pi in range(0, 2):
+                self.players[teams[2 * ci + ti, pi]].mmr_tag_w = min(max(self.players[teams[2 * ci + ti, pi]].mmr_tag_w, 0), 2)
+                self.players[teams[2 * ci + ti, pi]].mmr_tag_l = min(max(self.players[teams[2 * ci + ti, pi]].mmr_tag_l, 0), 2)
 
     def cor_res(self, res):
         self.state = 3